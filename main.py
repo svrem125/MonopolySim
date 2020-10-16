190cec9b570470085b14e6f3c98c3b5ac351b0e4
svrem125 <svrem@icloud.com>
2020-10-16 18:52:53 +0200

    data

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import random
+import json
+
+data = json.load(open('data.json'))
 
 class Player:
     def __init__(self, name):
@@ -12,7 +15,11 @@ class Player:
         target.money += amount
         
     def placeData(self):
-        res = next((sub for sub in monopolyData['tiles'] if sub['name'] == chance['tileid']), None) 
+        # print(self.position)
+        # res = next((sub for sub in data['properties'] if sub['position'] == self.position))
+        # print(res)
+        self.place = data['tiles'][self.position]
+         
     
     def throw(self):
         throw = (random.randint(0,6), random.randint(0, 6))
@@ -30,5 +37,10 @@ class Player:
         return players            
     
 
-players = Player.makePlayers(4)
+player1 = Player('1')
+player2 = Player('2')
+player3 = Player('3')
+player4 = Player('4')
 
+player2.throw()
+player2.placeData()
