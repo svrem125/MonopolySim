1c05682526b3c019bdb3db4f7c8de7bbd445bb7a
svrem125 <svrem@icloud.com>
2020-10-16 18:06:44 +0200

    Start

---
+++ main.py
@@ -0,0 +1,26 @@
+import random
+
+class Player:
+    def __init__(self, name):
+        self.name = name
+        self.money = 1500
+        self.position = 0
+        self.properties = []
+        
+    def pay(self, target, amount):
+        self.money -= amount
+        target.money += amount
+    
+    def throw(self):
+        throw = (random.randint(0,6), random.randint(0, 6))
+        self.position += sum(throw)
+        if throw[0] == throw[1]:
+            return True
+        else:
+            return False
+    
+
+player1 = Player('0')
+player2 = Player('1')
+
+player1.throw()
